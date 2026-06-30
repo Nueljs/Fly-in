2dfc4ad0cc9a1c93bb483e6924f3c39a4f1779c5
Nueljs <manuelcerveralara@gmail.com>
2026-06-30 18:40:52 +0200

    a

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from models import Network
+from models import Network, Zone
 
 
 class MapParser:
@@ -7,6 +7,17 @@ class MapParser:
         self.network: Network = Network()
         self.current_line: int = 0
         self.nb_drones: int = 0
+
+    def _parse_zone(self, line: str) -> Zone:
+        splited_data: list = line.split("[")
+        if len(splited_data) > 1:
+            metadata: str = splited_data[1]
+        data_list: list = [data.strip() for data in splited_data[0].split(" ")]
+        
+
+
+
+
 
     def parse(self) -> Network:
         with open(self.filepath, 'r') as f:
@@ -19,8 +30,13 @@ class MapParser:
                     continue
                 elif line.startswith("nb_drones"):
                     split_line = line.split(":")
-                    if int(split_line[1]) > 0:
-                        self.nb_drones = int(split_line[1])
+                    try:
+                        value_drone = int(split_line[1])
+                    except ValueError:
+                        raise ValueError(f"Error at line {self.current_line}: nb_drones must be an int")
+                    if value_drone > 0:
+                        self.nb_drones = value_drone
                     else:
                         raise ValueError(f"Error at line {self.current_line}:"
-                                          f"nb_drones must be greater than zero")
+                                        f"nb_drones must be greater than zero")
+
