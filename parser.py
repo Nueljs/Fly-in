7da2b8ef3f7f8d9ab520e5fa1993eba822709b6c
manuel <manuelcerveralara@gmail.com>
2026-07-01 04:44:36 +0200

    a

--- parser.py
+++ parser.py
@@ -12,12 +12,34 @@ class MapParser:
         splited_data: list = line.split("[")
         if len(splited_data) > 1:
             metadata: str = splited_data[1]
-        data_list: list = [data.strip() for data in splited_data[0].split(" ")]
-        
-
-
-
-
+            metadata.replace("]", "")
+            splited_metadata = metadata.split()
+            dict_metadata: dict = {}
+            for item in splited_metadata:
+                meta_list = item.split("=")
+                if len(meta_list) == 2:
+                    dict_metadata[meta_list[0]] = meta_list[1]
+                else:
+                    raise ValueError(f"Error on line {self.current_line}:"
+                                       " invalid format in metadata use [key=value]")
+            if "zone" in dict_metadata:
+                zone = dict_metadata["zone"]
+            if "color" in dict_metadata:
+                color = dict_metadata["color"]
+            if "max_drones" in dict_metadata:
+                try:
+                    max_drones = int(dict_metadata["max_drones"])
+                except ValueError:
+                    raise ValueError(f"Error on line f{self.current_line}: "
+                                     "max_drones must be an int")
+        data_list: list = [data.strip() for data in splited_data[0].split()]
+        zone_type, name, str_x, str_y = data_list
+        try:
+            x = int(str_x)
+            y = int(str_y)
+        except ValueError:
+            raise ValueError(f"Error on line {self.current_line},"
+                             " the coordenates must be a int")
 
     def parse(self) -> Network:
         with open(self.filepath, 'r') as f:
